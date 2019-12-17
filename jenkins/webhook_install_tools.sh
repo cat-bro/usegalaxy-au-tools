b99a9289e87ce08a94f177e0f553154534f903f8
cat-bro <cathjb@gmail.com>
2019-12-17 17:12:34 +1100

    .

--- jenkins/webhook_install_tools.sh
+++ jenkins/webhook_install_tools.sh
@@ -5,7 +5,7 @@ export AU_TOOLS_DIR=usegalaxy-au-tools
 #export AU_TOOLS_GIT=git@github.com:cat-bro/$AU_TOOLS_DIR.git
 
 export GALAXY_URL=https://cat-dev.genome.edu.au
-
+export LOCAL_TOOL_DIR=cat-dev
 export TOOL_FILE_PATH=$AU_TOOLS_DIR/galaxy-aust-dev/graphdisplay_data.yml
 
 echo Using variables
@@ -25,7 +25,7 @@ if [ ! -d $VIRTUALENV ]; then
 	cd workspace
 fi
 . $VIRTUALENV/bin/activate
-get-tool-list -g $GALAXY_URL -a $GALAXY_API_KEY -o installed_tools.yml
-python scripts/install_added_tools.py
+# get-tool-list -g $GALAXY_URL -a $GALAXY_API_KEY -o installed_tools.yml
+python scripts/install_added_tools.py -g $GALAXY_URL -a $GALAXY_API_KEY -d $LOCAL_TOOL_DIR
 #shed-tools install -g $GALAXY_URL -a $GALAXY_API_KEY -t $TOOL_FILE_PATH -v
 #rm -rf $AU_TOOL_DIR
--- scripts/install_added_tools.py
+++ scripts/install_added_tools.py
@@ -1,2 +1,28 @@
-print("Hello world")
- 
+#!/usr/bin/env python
+
+import yaml
+import argparse
+import os
+
+def main():
+
+    VERSION = 0.1
+
+    parser = argparse.ArgumentParser(description="Splits up a Ephemeris `get_tool_list` yml file for a Galaxy server into individual files for each Section Label.")
+    parser.add_argument("-g", "--galaxy_server", help="Galaxy server URL")
+    parser.add_argument("-a", "--api_key", help="API key for galaxy server")
+    paser.add_argument("-d", "--directory", help="Local directory of tool files")
+
+    args = parser.parse_args()
+
+    galaxy_server = args.galaxy_server
+    api_key = args.api_key
+    dir = args.directory
+    tools_output_dir = 'tmp/installed_tools.yml'
+
+    repository_state = [] # list of yamls, there must be a better way
+    files = os.listdir(dir)
+    for file in files:
+        repository_state.append(yaml.load('dir/' + file))
+
+    print(files)
