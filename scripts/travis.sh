163c6d21e60c0bc26df89d571bf845ccc83922d4
cat-bro <cathjb@gmail.com>
2020-01-06 17:05:57 +1100

    .

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -12,7 +12,7 @@ if [ ! $TRAVIS_PULL_REQUEST ]; then
 fi
 
 # check the range of the commit input_file_paths
-CHANGED_FILES=$(git diff --name-only HEAD...$TRAVIS_BRANCH)
+CHANGED_FILES=$(git diff --name-only $TRAVIS_BRANCH master)
 echo ________________
 echo $CHANGED_FILES
 echo ________________
